49fcfde2eabc470b2288252c822cf51a82f33f0e
Rassek05 <fabio.enrique14@gmail.com>
2026-05-29 12:08:34 -0400

    add-more-options

--- index.js
+++ index.js
@@ -16,36 +16,41 @@ form.addEventListener("submit", async (event) => {
   ).value;
   const amountValue = amount.value;
   console.log(coinSelected, cryptoSelected, amountValue);
-
   try {
-        coinInfo.style.display = 'block';
-        coinInfo.innerHTML = `
+    coinInfo.style.display = "block";
+    coinInfo.innerHTML = `
         <div class="loader"></div>
-        `; 
-        const response = await (await fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSelected}&tsyms=${coinSelected}`)).json();
-        const price = response.DISPLAY[cryptoSelected][coinSelected].PRICE;
-        const priceHigh = response.DISPLAY[cryptoSelected][coinSelected].HIGH24HOUR;
-        const priceLow = response.DISPLAY[cryptoSelected][coinSelected].LOW24HOUR;
-        const variation = response.DISPLAY[cryptoSelected][coinSelected].CHANGEPCT24HOUR;
-        console.log(response.RAW[cryptoSelected][coinSelected].PRICE);
+        `;
+    const response = await (
+      await fetch(
+        `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSelected}&tsyms=${coinSelected}`,
+      )
+    ).json();
+    const price = response.DISPLAY[cryptoSelected][coinSelected].PRICE;
+    const priceHigh = response.DISPLAY[cryptoSelected][coinSelected].HIGH24HOUR;
+    const priceLow = response.DISPLAY[cryptoSelected][coinSelected].LOW24HOUR;
+    const variation =
+      response.DISPLAY[cryptoSelected][coinSelected].CHANGEPCT24HOUR;
+    console.log(response.RAW[cryptoSelected][coinSelected].PRICE);
 
-        if (amountValue !== '') {
-            const result = Number(amountValue) / response.RAW[cryptoSelected][coinSelected].PRICE;
-            coinInfo.innerHTML = `
+    if (amountValue !== "") {
+      const result =
+        Number(amountValue) / response.RAW[cryptoSelected][coinSelected].PRICE;
+      coinInfo.innerHTML = `
         <p class="info">El precio es: <span class="price">${price}</span></p>
         <p class="info">El precio mas alto es: <span class="price">${priceHigh}</span></p>
         <p class="info">El precio mas bajo es: <span class="price">${priceLow}</span></p>
         <p class="info">Variacion 24H: <span class="price">${variation}%</span></p>
         <p class="info">Puede comprar: <span class="price">${result.toFixed(4)} ${cryptoSelected}</span></p>
         `;
-        } else {
-            coinInfo.innerHTML = `
+    } else {
+      coinInfo.innerHTML = `
             <p class="info">El precio es: <span class="price">${price}</span></p>
             <p class="info">El precio mas alto es: <span class="price">${priceHigh}</span></p>
             <p class="info">El precio mas bajo es: <span class="price">${priceLow}</span></p>
             <p class="info">Variacion 24H: <span class="price">${variation}%</span></p>
         `;
-        }
+    }
   } catch (error) {
     console.log(error);
   }
